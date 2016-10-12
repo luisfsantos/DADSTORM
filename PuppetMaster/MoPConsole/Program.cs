3cad2f7b06e2668518feb0c35ef930e1ee5b35dc
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-10-12 19:09:56 +0100

    init: puppet master initial config

---
+++ PuppetMaster/MoPConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using DADSTORM.PuppetMaster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoPConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PuppetMaster MoP = new PuppetMaster();
+            MoP.start(10);
+            MoP.crash("tcp://1.2.3.4:5650/OP");
+            Console.ReadLine();
+        }
+    }
+}
