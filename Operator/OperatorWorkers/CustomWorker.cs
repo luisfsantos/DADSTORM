bb60facf5f9454a1b5da4deec5141f6f94a2ce65
Catarina Cepeda <catarinacepeda@gmail.com>
2016-11-11 12:52:46 +0000

    Implement CustomWorker

--- Operator/OperatorWorkers/CustomWorker.cs
+++ Operator/OperatorWorkers/CustomWorker.cs
@@ -1,24 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
-namespace DADSTORM.Operator.OperatorWorkers
-{
+namespace DADSTORM.Operator.OperatorWorkers {
     class CustomWorker : OperatorWorker {
         private string Dll;
         private string Class;
         private string Method;
 
         public CustomWorker(Operator op, string dll, string @class, string method) : base (op){
-            this.Dll = dll;
-            this.Class = @class;
-            this.Method = method;
+            this.Dll = dll.Trim('"');
+            this.Class = @class.Trim('"');
+            this.Method = method.Trim('"');
         }
 
         public override void processTuple(List<string> tuple) {
-            throw new NotImplementedException();
+            byte[] code = File.ReadAllBytes(Dll);
+            Assembly assembly = Assembly.Load(code);
+
+            foreach (Type type in assembly.GetTypes()) {
+                if (type.IsClass) {
+                    if (type.FullName.EndsWith("." + Class)) {
+                        object ClassObj = Activator.CreateInstance(type);
+
+                        object[] args = new object[] { tuple };
+                        object resultObject = type.InvokeMember(Method, 
+                            BindingFlags.Default | BindingFlags.InvokeMethod, 
+                            null, 
+                            ClassObj, 
+                            args);
+
+                        IList<List<string>> result = (IList<List<string>>)resultObject;
+
+                        foreach (List<string> resultTuple in result) {
+                            Op.addTupleToSend(resultTuple);
+                        }
+                        break;
+                    }
+                }
+            }
         }
     }
 }
